f1c2cff6f1fe104388c1de80f2e777b005d21faf
talvi-frsch <144930659+talvi-frsch@users.noreply.github.com>
2023-10-20 21:52:45 +0300

    Create main.go

---
+++ contest_02/09/main.go
@@ -0,0 +1,34 @@
+package main
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
+//Мой код
+import "strconv"
+
+func isLucky(number string) bool {
+	l := 0
+	r := 0
+
+	for _, str := range number[:3] {
+		temp, _ := strconv.Atoi(string(str))
+		l += temp
+	}
+	
+	for _, str := range number[3:] {
+		temp, _ := strconv.Atoi(string(str))
+		r += temp
+	}
+	
+	return l == r
+}
+//Конец моего кода
+func main() {
+    number, _  := bufio.NewReader(os.Stdin).ReadString('\n')
+    if isLucky( number[:len(number)-1] ){  // Обрезаем \n в конце строки
+        fmt.Println("YES")
+    }else{
+        fmt.Println("NO")
+    }
+}

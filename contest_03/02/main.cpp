d0cdda828ab435a3393a2ddfe09124df56d64fa0
talvi-frsch <144930659+talvi-frsch@users.noreply.github.com>
2023-10-22 18:49:03 +0300

    Update main.cpp

--- contest_03/02/main.cpp
+++ contest_03/02/main.cpp
@@ -1,1 +1,59 @@
-2
+#include <iostream>
+#include <string>
+
+using namespace std;
+string get_string(char end = '\n') {
+	string text;
+	getline(cin, text, end);
+	return text;
+}
+bool predicate(string text) {
+	int count1 = 0, count2 = 0, count3 = 0;
+	string prev_sym = "";
+	for (char elem : text) {
+		if (count1 < 0 || count2 < 0 || count3 < 0) return false;
+		if (elem == '(') {
+			count1++;
+			prev_sym = ")" + prev_sym;
+			continue;
+		}
+		if (elem == ')') {
+			count1--;
+			if (prev_sym[0] != elem) return false;
+			prev_sym = prev_sym.substr(1);
+			continue;
+		}
+
+		if (elem == '[') {
+			count2++;
+			prev_sym = "]" + prev_sym;
+			continue;
+
+		}
+		if (elem == ']') {
+			count2--;
+			if (prev_sym[0] != elem) return false;
+			prev_sym = prev_sym.substr(1);
+			continue;
+
+		}
+		if (elem == '{') {
+			count3++;
+			prev_sym = "}" + prev_sym;
+			continue;
+
+		}
+		if (elem == '}') {
+			count3--;
+			if (prev_sym[0] != elem) return false;
+			prev_sym = prev_sym.substr(1);
+			continue;
+		}
+	}
+	return count1 == 0 && count2 == 0 && count3 == 0;
+}
+
+int main() {
+	string text = get_string('!');
+	predicate(text) ? cout << "YES" : std::cout << "NO";
+}

a1f35a6f2683498e3fa61656b2fa026183c00b00
talvi-frsch <144930659+talvi-frsch@users.noreply.github.com>
2024-01-10 17:16:33 +0300

    Update main.cpp

--- contest_08/01/main.cpp
+++ contest_08/01/main.cpp
@@ -1,3 +1,22 @@
+#include <iostream>
+
+class Keeper{
+    int trash;
+    int secret;
+    
+public:
+    Keeper(){
+        std::cin >> secret;
+    }
+};
+
+// Ваш код здесь
 int hack_it(Keeper& keeper){
 	return *(reinterpret_cast<int*>(&keeper) + 1);
+}
+
+int main(){
+    Keeper keeper;
+    int secret= hack_it(keeper);
+    std::cout << secret;
 }
